b6b84e511d37264f9091357963ff996322ec7e2e
gunpyo0908 <gunpyo0908@gmail.com>
2024-12-04 00:51:59 +0900

    feat : coin toss 100번하기 - 100번해서 앞뒤를 셈

--- project.c
+++ project.c
@@ -9,6 +9,18 @@ int main(void)
 
     srand((unsigned)time(NULL));
 
+        for(toss=0; toss<100; toss++)
+    {
+        if(coin_toss() == 1)
+        {
+            front++;
+        }
+        else
+        {
+            back++;
+        }
+    }
+
     printf("동전의 앞면: %d \n", front);
     printf("동전의 뒷면: %d \n", back);
 
